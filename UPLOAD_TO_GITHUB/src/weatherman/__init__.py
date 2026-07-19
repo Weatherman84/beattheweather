4b3dda7e26776d258bf5c1b12db7f22b891afb36
Weatherman84 <stefan.fleck@outlook.com>
2026-07-19 14:27:24 +0200

    Delete UPLOAD_TO_GITHUB directory

--- UPLOAD_TO_GITHUB/src/weatherman/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Weatherman temperature-market analytics."""
-
-__version__ = "1.0.0"
--- UPLOAD_TO_GITHUB/tests/test_settings.py
+++
@@ -1,7 +0,0 @@
-from weatherman.settings import airports
-
-
-def test_packaged_airports_are_available():
-    catalog = airports()
-    assert set(catalog) == {"LEMD", "EHAM", "EPWA", "LTAC"}
-    assert "ukmo_global_deterministic_10km" in catalog["LEMD"]["models"]
